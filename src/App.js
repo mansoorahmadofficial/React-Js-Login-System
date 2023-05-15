1def6b9a6715867f742b2164518172899e2d7ecd
mansoorahmadofficial <coddingcity@gmail.com>
2023-05-15 23:44:38 +0500

    update table data

--- src/App.js
+++ src/App.js
@@ -1,36 +1,41 @@
 import React, { useState } from "react";
 
 const App = () => {
+  const [loginForm, setLoginForm] = useState({ username: "", password: "" });
+  const [dataList, setDataList] = useState([]);
 
-  const [loginForm, setLoginForm] = useState({username:'',password:''});
-  const handleFormSubmit = (event)=> {
+  const handleFormSubmit = (event) => {
     event.preventDefault();
-    console.log(loginForm.username);
-    if(loginForm.username ==='' && loginForm.password ===''){
+    // console.log(loginForm.username);
+    if (loginForm.username === "" && loginForm.password === "") {
       alert("Username and Password required");
     }
-  }
-  const handleFormChange = (event)=>{
+    setDataList([...dataList, loginForm]);
+    setLoginForm("");
+    
+   event.target.reset();
+    // console.log(dataList);
+  };
+  const handleFormChange = (event) => {
     const name = event.target.name;
     const value = event.target.value;
-    setLoginForm({...loginForm,[name]:value});
-    console.log({...loginForm,[name]:value})
-  }
+    setLoginForm({ ...loginForm, [name]: value });
+    // console.log({...loginForm,[name]:value})
+  };
   return (
     <div className="container">
-      <div className="row" style={{margin:20}}>
+      <div className="row" style={{ margin: 20 }}>
         <div
           className="col-md-4 offset-md-4 col-sm-10 offset-sm-1 col-xs-10 offset-xs-1"
           style={{
-            margin:'150px auto',
+            margin: "150px auto",
             backgroundColor: "#f1f1f1",
             paddingBottom: 50,
             borderRadius: 30,
             border: "0.5px solid #ccc",
             boxShadow: "0px 3px lightblue",
-            textAlign:"center"
+            textAlign: "center",
           }}
-          
         >
           <div className="col-md-12 center text-center" style={{ padding: 15 }}>
             <img
@@ -47,7 +52,7 @@ const App = () => {
                 aria-describedby="username"
                 placeholder="Username"
                 autoComplete="Off"
-                style={{fontSize:15}}
+                style={{ fontSize: 15 }}
                 onChange={handleFormChange}
               />
             </div>
@@ -57,7 +62,7 @@ const App = () => {
                 className="form-control"
                 name="password"
                 placeholder="Password"
-                style={{fontSize:15}}
+                style={{ fontSize: 15 }}
                 onChange={handleFormChange}
               />
             </div>
@@ -66,6 +71,28 @@ const App = () => {
               Login
             </button>
           </form>
+          <div className="col-md-12">
+            <table className="table table-hover">
+              <thead>
+                <tr>
+                  <th>#</th>
+                  <th>Username</th>
+                  <th>Password</th>
+                  <th>Action</th>
+                </tr>
+              </thead>
+              <tbody>
+                {dataList.map((data, index) => (
+                  <tr key={index}>
+                    <td>{index==0 ? index+1 : index+1}</td>
+                    <td>{data.username}</td>
+                    <td>{data.password}</td>
+                    <td><button className="btn btn-danger">Delete</button></td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
         </div>
       </div>
     </div>
